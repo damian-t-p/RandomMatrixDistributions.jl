56819cde37468eef68734c033e95c2a525fa4eaa
damian-t-p <damian.pavlyshyn@gmail.com>
2019-05-21 16:15:01 -0700

    Improve Tracy-Widom quantile edges
    The Tracy-Widom quantile method now only interpolates in the middle of
    the distribution, while known limiting behaviour is used for the edges
    near 0 and 1.
    
    Also added argument descriptions to README.org.

--- src/densities/TracyWidom.jl
+++ src/densities/TracyWidom.jl
@@ -4,7 +4,7 @@ struct TracyWidom <: ContinuousUnivariateDistribution
     beta::Integer
 end
 
-function cdf(d::TracyWidom, x)
+function cdf(d::TracyWidom, x::Real)
     cdf(RandomMatrices.TracyWidom, x, beta=d.beta)
 end
 
@@ -19,23 +19,48 @@ function _TWf(x, beta)
     _TWfs[beta](x)
 end
 
-function pdf(d::TracyWidom, x)
+function pdf(d::TracyWidom, x::Real)
     _TWf(x, d.beta)
 end
 
 _TWinvs = Dict()
 
 function _TWinv(t, beta)
+    (0 < t < 1) || throw(DomainError(t))
+
     if !(beta in keys(_TWinvs))
+        # bounds outside which we use limiting representation
+        u = 4
+        l = -5
+        
         # TODO: make a more principled grid using tail behaviour
         n = 1000
-        y = range(-5, 4, length=n)
+        y = range(l, u, length=n)
         x = cdf(RandomMatrices.TracyWidom, y, beta=1)
 
+        # Transform limiting ends to maintain differentiability
+        righta = pdf(TracyWidom(beta), u)/(beta * sqrt(u) * exp(-beta * u^(3/2)*2/3))
+        rightb = x[end] - righta * (1 - exp(-beta * u^(3/2) * 2/3))
+
+        lefta = pdf(TracyWidom(beta), l)/(-beta * l/12 * exp(-beta * l^2/24))
+        leftb = x[1] - lefta * exp(-beta * l^2/24)
+
         itp = interpolate((x,), y, Gridded(Linear()))
-        _TWinvs[beta] = Interpolations.extrapolate(itp, Interpolations.Line())
+
+        _TWinvs[beta] = [righta, rightb, x[end], lefta, leftb, x[1], itp]
     end
-    _TWinvs[beta](t)
+
+    # TODO: make the indexing less terrible
+    if t > _TWinvs[beta][3]
+        s = (t - _TWinvs[beta][2])/_TWinvs[beta][1]
+        return (log(1/(1 - s)) * 3/(2 * beta))^(2/3)
+    elseif t < _TWinvs[beta][6]
+        s = (t - _TWinvs[beta][5])/_TWinvs[beta][4]
+        return -sqrt(log(1/s) * 24/beta)
+    else
+        return _TWinvs[beta][end](t)
+    end
+        
 end
 
 function quantile(d::TracyWidom, q::Real)
