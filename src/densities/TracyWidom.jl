fe5481fa85e1a731ce3adf0768780fbd5f338923
damian-t-p <damian.pavlyshyn@gmail.com>
2019-05-20 18:41:06 -0700

    Implement quantiles for Tracy-Widom distribution
    Pretty naive approach, using a linear interpolation of points sampled
    uniformly from the y-axis. Works well enough, but there's work to do
    about cleaning up the tails.

--- src/densities/TracyWidom.jl
+++ src/densities/TracyWidom.jl
@@ -4,11 +4,11 @@ struct TracyWidom <: ContinuousUnivariateDistribution
     beta::Integer
 end
 
-_TWfs = Dict()
-
 function cdf(d::TracyWidom, x)
     cdf(RandomMatrices.TracyWidom, x, beta=d.beta)
 end
+
+_TWfs = Dict()
 
 function _TWf(x, beta)
     if !(beta in keys(_TWfs))
@@ -21,5 +21,24 @@ end
 
 function pdf(d::TracyWidom, x)
     _TWf(x, d.beta)
+end
+
+_TWinvs = Dict()
+
+function _TWinv(t, beta)
+    if !(beta in keys(_TWinvs))
+        # TODO: make a more principled grid using tail behaviour
+        n = 1000
+        y = range(-5, 4, length=n)
+        x = cdf(RandomMatrices.TracyWidom, y, beta=1)
+
+        itp = interpolate((x,), y, Gridded(Linear()))
+        _TWinvs[beta] = Interpolations.extrapolate(itp, Interpolations.Line())
+    end
+    _TWinvs[beta](t)
+end
+
+function quantile(d::TracyWidom, q::Real)
+    _TWinv(q, d.beta)
 end
 
