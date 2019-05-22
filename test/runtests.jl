411927b6aa3c0b30c2b223cbda3ae56f9d1375b7
damian-t-p <damian.pavlyshyn@gmail.com>
2019-05-22 14:23:05 -0700

    Conform to package registration requirements
    Added [compat] entries to Project.toml for julia and external
    dependencies.
    
    Added rudimentary test suite for Wishart and Jacobi samplers.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,36 @@
+using RandomMatrixDistributions
 using Test
 
-@test 1 == 1
+n = 1000
+p = 400
+γ = p/n
+
+# test white Wishart
+for beta in [1, 4, 8]
+    λs = randeigvals(SpikedWishart(beta, n, p))
+
+    @test length(λs) == p
+    @test eltype(λs) <: Real
+
+    @test minimum(λs) > 0
+
+    λs = randeigvals(Jacobi(beta, n, n, p))
+
+    @test length(λs) == p
+    @test eltype(λs) <: Real
+
+    @test minimum(λs) > 0
+end
+
+s = 8
+λs = randeigvals(SpikedWishart(1, n, p, [2, 4, s]))/n
+
+@test length(λs) == p
+@test eltype(λs) <: Real
+
+# Check if the maximum spike is reasonable
+# This tests can fail in principle, but only with negligible probability
+μ = (1 + s) * (1 + γ/s)
+σ = (1 + s) * sqrt(γ * 2 * (1 - γ/s^2))
+
+@test abs(maximum(λs) - μ)/σ < 4
